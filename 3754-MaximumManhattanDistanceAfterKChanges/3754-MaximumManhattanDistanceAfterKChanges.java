18cbe253c9fbf671359ef1d5b1f618e663d595e2
RAGA SANDHIYA R <ragasandhiya05@gmail.com>
2026-07-09 09:47:58 +0530

    Create 3754-MaximumManhattanDistanceAfterKChanges

---
+++ 3754-MaximumManhattanDistanceAfterKChanges/3754-MaximumManhattanDistanceAfterKChanges.java
@@ -0,0 +1,20 @@
+// Last updated: 7/9/2026, 9:47:56 AM
+class Solution {
+    public int maxDistance(String s, int k) {
+        int n = s.length();
+        int netY = 0 , netX = 0;
+        int dist = 0;
+        for(int i = 0 ; i < n ; i++) {
+            char c = s.charAt(i);
+            switch(c) {
+                case 'N' : netY++; break;
+                case 'S' : netY--; break;
+                case 'E' : netX++; break;
+                case 'W' : netX--; break;
+            }
+            int d = Math.abs(netY) + Math.abs(netX) + 2 * k;
+            dist = Math.max(dist , Math.min(d , i+ 1));
+        }
+        return dist;
+    }
+}
