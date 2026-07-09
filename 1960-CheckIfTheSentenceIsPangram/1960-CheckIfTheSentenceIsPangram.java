9c4f9423b45b8546715e5f74177a12d2e734926e
RAGA SANDHIYA R <ragasandhiya05@gmail.com>
2026-07-09 09:53:03 +0530

    Create 1960-CheckIfTheSentenceIsPangram

---
+++ 1960-CheckIfTheSentenceIsPangram/1960-CheckIfTheSentenceIsPangram.java
@@ -0,0 +1,14 @@
+// Last updated: 7/9/2026, 9:53:01 AM
+class Solution {
+    public boolean checkIfPangram(String sentence) {
+        if(sentence.length() < 26)
+        return false;
+        for(char ch = 'a' ; ch <= 'z' ; ch++){
+            if(sentence.indexOf(ch) < 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
+           
