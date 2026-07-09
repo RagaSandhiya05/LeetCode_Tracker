c45cc3acddf3f8dec7868de905d87a0c5a7cfaf1
RAGA SANDHIYA R <ragasandhiya05@gmail.com>
2026-07-09 09:53:53 +0530

    Create 1704-SpecialPositionsInABinaryMatrix

---
+++ 1704-SpecialPositionsInABinaryMatrix/1704-SpecialPositionsInABinaryMatrix.java
@@ -0,0 +1,28 @@
+// Last updated: 7/9/2026, 9:53:52 AM
+// Special Position in a Binary Matrix
+class Solution {
+    public int numSpecial(int[][] mat) {
+        int m = mat.length , n = mat[0].length;
+        int row[] = new int[m];
+        int cols[] = new int[n];
+        for(int i = 0 ; i < m ; i++) {
+                for(int j = 0 ; j < n ; j++) {
+                    if(mat[i][j] == 1) {
+                        row[i]++;
+                        cols[j]++;
+                    }
+                }
+        }
+            int ans = 0;
+            for(int i = 0 ; i < m ; i++) {
+                for(int j = 0 ; j < n ; j++) {
+                    if(mat[i][j] == 1 && row[i] == 1 && cols[j] == 1) {
+                        ans++;
+                    }
+                }
+            }
+            return ans;
+        }
+    }
+
+                    
