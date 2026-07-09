3649182fd1e28431aa4f855e93ac6f05b7140c55
RAGA SANDHIYA R <ragasandhiya05@gmail.com>
2026-07-09 10:01:14 +0530

    Create 70-ClimbingStairs

---
+++ 70-ClimbingStairs/70-ClimbingStairs.java
@@ -0,0 +1,18 @@
+// Last updated: 7/9/2026, 10:01:12 AM
+// Climbing Stairs
+class Solution {
+    public int climbStairs(int n) {
+        if(n == 0 || n == 1) {
+            return 1;
+        }
+        int prev = 1;
+        int curr = 1;
+        for(int i = 2 ; i <= n ; i++) {
+            int temp = curr;
+            curr = prev + curr;
+            prev = temp;
+        }
+        return curr;
+    }
+}
+      
