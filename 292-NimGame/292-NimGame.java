fe00a04d6ce5802b1b9d565a725c8cf772df2cb8
RAGA SANDHIYA R <ragasandhiya05@gmail.com>
2026-07-09 09:58:05 +0530

    Create 292-NimGame

---
+++ 292-NimGame/292-NimGame.java
@@ -0,0 +1,10 @@
+// Last updated: 7/9/2026, 9:58:04 AM
+// Nim Game
+class Solution {
+    public boolean canWinNim(int n) {
+        if(n % 4 != 0) 
+            return true;
+        else
+            return false;
+    }
+}
