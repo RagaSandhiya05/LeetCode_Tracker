e433a8915231426de235ead74b79a88e9d6c2b16
RAGA SANDHIYA R <ragasandhiya05@gmail.com>
2026-07-09 09:46:52 +0530

    Create 3967-EarliestFinishTimeForLandAndWaterRidesIi

---
+++ 3967-EarliestFinishTimeForLandAndWaterRidesIi/3967-EarliestFinishTimeForLandAndWaterRidesIi.java
@@ -0,0 +1,19 @@
+// Last updated: 7/9/2026, 9:46:51 AM
+// Earliest Finish Time for Land and Water Rides II
+class Solution {
+    private int solve(int[] start1 , int[] duration1 , int[] start2 , int[] duration2) {
+        int earlierFinish = Integer.MAX_VALUE;
+        for(int i = 0 ; i < start1.length ; i++) {
+            earlierFinish = Math.min(earlierFinish , start1[i] + duration1[i]);
+        }
+        int ans = Integer.MAX_VALUE;
+        for(int i = 0 ; i < start2.length ; i++) {
+            ans = Math.min(ans , Math.max(earlierFinish , start2[i]) + duration2[i]);
+        }
+        return ans;
+        }
+    public int earliestFinishTime(int[] landStartTime, int[] landDuration, int[] waterStartTime, int[] waterDuration) {
+        return Math.min(solve(landStartTime , landDuration , waterStartTime , waterDuration) , 
+        solve(waterStartTime , waterDuration , landStartTime , landDuration));
+    }
+}
