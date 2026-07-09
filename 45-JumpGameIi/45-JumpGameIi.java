3c9bf8ceaa100e05dffb061ad7e1786e803fc7cf
RAGA SANDHIYA R <ragasandhiya05@gmail.com>
2026-07-09 10:01:35 +0530

    Create 45-JumpGameIi

---
+++ 45-JumpGameIi/45-JumpGameIi.java
@@ -0,0 +1,19 @@
+// Last updated: 7/9/2026, 10:01:34 AM
+// Jump Game II
+class Solution {
+    public int jump(int[] nums) {
+        int n = nums.length;
+        int jumps = 0;
+        int CurrentEnd = 0;
+        int Farthest = 0;
+        for(int i = 0 ; i < n - 1 ; i++) {
+            Farthest = Math.max(Farthest , i + nums[i]);
+            if(i == CurrentEnd) {
+                jumps++;
+                CurrentEnd = Farthest;
+            }
+        }
+        return jumps;
+    }
+}
+           
