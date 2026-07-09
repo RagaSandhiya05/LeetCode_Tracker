2852799336752463b5cbc0d0f5a74f65a0f94b56
RAGA SANDHIYA R <ragasandhiya05@gmail.com>
2026-07-09 09:55:40 +0530

    Create 920-UncommonWordsFromTwoSentences

---
+++ 920-UncommonWordsFromTwoSentences/920-UncommonWordsFromTwoSentences.java
@@ -0,0 +1,19 @@
+// Last updated: 7/9/2026, 9:55:39 AM
+// Uncommon Words from Two Sentences
+class Solution {
+    public String[] uncommonFromSentences(String s1 , String s2) {
+        String str = s1 + " " + s2;
+        Map<String, Integer> wordCount = new HashMap<>();
+        String[] words = str.split(" ");
+        for (String word : words) {
+            wordCount.put(word , wordCount.getOrDefault(word , 0) + 1);
+        }
+        List<String> result = new ArrayList<>();
+        for (Map.Entry<String , Integer> entry : wordCount.entrySet()) {
+            if (entry.getValue() == 1) {
+                result.add(entry.getKey());
+            }
+        }
+        return result.toArray(new String[0]);
+    }
+}
