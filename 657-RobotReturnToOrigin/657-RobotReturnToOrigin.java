dfe5592ea9985f48c4f5592a8871a8b17c38f2a1
RAGA SANDHIYA R <ragasandhiya05@gmail.com>
2026-07-09 09:56:20 +0530

    Create 657-RobotReturnToOrigin

---
+++ 657-RobotReturnToOrigin/657-RobotReturnToOrigin.java
@@ -0,0 +1,15 @@
+// Last updated: 7/9/2026, 9:56:19 AM
+// Robot Return to Origin
+class Solution {
+    public boolean judgeCircle(String moves) {
+        int count_v = 0, count_h = 0;
+        for (int i = 0; i < moves.length(); i++) {
+            char ch = moves.charAt(i);
+            if (ch == 'U') count_v++;
+            else if (ch == 'D') count_v--;
+            else if (ch == 'R') count_h++;
+            else count_h--;
+        }
+        return count_v == 0 && count_h == 0;
+    }
+}
