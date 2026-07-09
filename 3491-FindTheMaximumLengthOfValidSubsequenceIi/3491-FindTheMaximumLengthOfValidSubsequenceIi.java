b7bbc513b900224f4da8faa5f505ecdc83a1aca7
RAGA SANDHIYA R <ragasandhiya05@gmail.com>
2026-07-09 09:48:40 +0530

    Create 3491-FindTheMaximumLengthOfValidSubsequenceIi

---
+++ 3491-FindTheMaximumLengthOfValidSubsequenceIi/3491-FindTheMaximumLengthOfValidSubsequenceIi.java
@@ -0,0 +1,24 @@
+// Last updated: 7/9/2026, 9:48:38 AM
+import java.util.*;
+
+class Solution {
+    public int maximumLength(int[] nums, int k) {
+        int maxLength = 2;
+
+        for (int targetMod = 0; targetMod < k; targetMod++) {
+            int[] remainderDP = new int[k];
+
+            for (int num : nums) {
+                int numMod = num % k;
+                int requiredMod = (targetMod - numMod + k) % k;
+                remainderDP[numMod] = remainderDP[requiredMod] + 1;
+            }
+
+            for (int length : remainderDP) {
+                maxLength = Math.max(maxLength, length);
+            }
+        }
+
+        return maxLength;
+    }
+}
