07b146bcf6754cea9bf3aff90f11d3dea33cabf4
RAGA SANDHIYA R <ragasandhiya05@gmail.com>
2026-07-09 09:47:10 +0530

    Create 3931-ProcessStringWithSpecialOperationsI

---
+++ 3931-ProcessStringWithSpecialOperationsI/3931-ProcessStringWithSpecialOperationsI.java
@@ -0,0 +1,26 @@
+// Last updated: 7/9/2026, 9:47:09 AM
+// Process String With Special Operations I
+class Solution {
+    public String processStr(String s) {
+        StringBuilder sb = new StringBuilder();
+        int n = s.length();
+        for(int i = 0 ; i < s.length() ; i++) {
+            char ch = s.charAt(i);
+            if(ch == '*') {
+                if(sb.length() != 0) {
+                    sb.deleteCharAt(sb.length() - 1);
+                }
+            }
+            else if(ch == '#') {
+                sb.append(sb);
+            }
+            else if(ch == '%') {
+                sb.reverse();
+            }
+            else if(ch >= 'a' && ch <= 'z') {
+                sb.append(ch);
+            }
+        }
+        return sb.toString();
+    }
+}
