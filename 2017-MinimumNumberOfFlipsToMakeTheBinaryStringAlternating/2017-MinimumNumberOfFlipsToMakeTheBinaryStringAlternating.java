ed95d3ad594ca0fc15553f0cac4ea1870c166217
RAGA SANDHIYA R <ragasandhiya05@gmail.com>
2026-07-09 09:52:45 +0530

    Create 2017-MinimumNumberOfFlipsToMakeTheBinaryStringAlternating

---
+++ 2017-MinimumNumberOfFlipsToMakeTheBinaryStringAlternating/2017-MinimumNumberOfFlipsToMakeTheBinaryStringAlternating.java
@@ -0,0 +1,23 @@
+// Last updated: 7/9/2026, 9:52:44 AM
+// Minimum Number of Flips to Make the Binary String Alternating
+class Solution {
+    public int minFlips(String s) {
+        String alt = "01";
+        int n = s.length();
+        int cnt = 0;
+        for(int i = 0; i < n; ++i) {
+            if(s.charAt(i) != alt.charAt(i & 1)) cnt++;
+        }
+        int flips = Math.min(cnt, n - cnt);
+        for(int i = 0; i < n; ++i){
+            if(s.charAt(i) != alt.charAt(i & 1)) {
+                --cnt;
+            }
+            if(s.charAt(i) != alt.charAt((i + n) & 1)){
+                ++cnt;
+            }    
+            flips = Math.min(flips, Math.min(cnt, n - cnt));
+        }
+        return flips;
+    }
+}
