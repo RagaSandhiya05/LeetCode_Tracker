5c9e01725fee750d54dee6803cc865101b86401a
RAGA SANDHIYA R <ragasandhiya05@gmail.com>
2026-07-09 09:46:45 +0530

    Create 3980-BestTimeToBuyAndSellStockUsingStrategy

---
+++ 3980-BestTimeToBuyAndSellStockUsingStrategy/3980-BestTimeToBuyAndSellStockUsingStrategy.java
@@ -0,0 +1,29 @@
+// Last updated: 7/9/2026, 9:46:44 AM
+class Solution {
+    public long maxProfit(int[] prices, int[] strategy, int k) {
+        int n = prices.length;
+        long profitPrefix[] = new long[n];
+        long pricePrefix[] = new long[n];
+        profitPrefix[0] = (long) prices[0] * strategy[0];
+        pricePrefix[0] = prices[0];
+        for(int i = 1 ; i < n ; ++i){
+            profitPrefix[i] = profitPrefix[i-1] + (long)prices[i]*strategy[i];
+            pricePrefix[i] = pricePrefix[i-1]+prices[i];
+        }
+        long baseProfit = profitPrefix[n-1];
+        long res = baseProfit;
+        int i = 0 , j = k-1;
+        int mid = k / 2;
+        while(j < n){
+            long oldWindow = i == 0 ? profitPrefix[j] : profitPrefix[j] - profitPrefix[i-1] ;
+            long newWindow = mid == 0 ? pricePrefix[j] : pricePrefix[j] - pricePrefix[mid-1];
+            long newSum = baseProfit - oldWindow + newWindow;
+            res = Math.max(res,newSum);
+            ++i;
+            ++mid;
+            ++j;
+        }
+        return res;
+
+    }
+}
