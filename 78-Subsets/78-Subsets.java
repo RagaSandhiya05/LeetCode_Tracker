3f9a280b9815ae4e29040aed8ec19129ba018698
RAGA SANDHIYA R <ragasandhiya05@gmail.com>
2026-07-09 10:01:08 +0530

    Create 78-Subsets

---
+++ 78-Subsets/78-Subsets.java
@@ -0,0 +1,17 @@
+// Last updated: 7/9/2026, 10:01:07 AM
+// Subsets
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> outer = new ArrayList<>();
+        outer.add(new ArrayList<>());
+        for(int num : nums) {
+            int n = outer.size();
+            for(int i = 0 ; i < n ; i++) {
+                List<Integer> internal = new ArrayList<>(outer.get(i));
+                internal.add(num);
+                outer.add(internal);
+            }
+        }
+        return outer;
+    }
+}
