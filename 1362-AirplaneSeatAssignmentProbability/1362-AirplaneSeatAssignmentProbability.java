b0a4f14e30ab8ca851cce724fbc8814813d1fbee
RAGA SANDHIYA R <ragasandhiya05@gmail.com>
2026-07-09 09:55:02 +0530

    Create 1362-AirplaneSeatAssignmentProbability

---
+++ 1362-AirplaneSeatAssignmentProbability/1362-AirplaneSeatAssignmentProbability.java
@@ -0,0 +1,7 @@
+// Last updated: 7/9/2026, 9:55:00 AM
+// Airplane Seat Assignment Probability
+class Solution {
+    public double nthPersonGetsNthSeat(int n) {
+        return n == 1 ? 1.0 : 0.5;
+    }
+}
