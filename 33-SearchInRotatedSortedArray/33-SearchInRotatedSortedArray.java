0efc31f21a2fe46ddb36ba5b7269fa1cc7fa1e78
RAGA SANDHIYA R <ragasandhiya05@gmail.com>
2026-07-09 10:01:44 +0530

    Create 33-SearchInRotatedSortedArray

---
+++ 33-SearchInRotatedSortedArray/33-SearchInRotatedSortedArray.java
@@ -0,0 +1,29 @@
+// Last updated: 7/9/2026, 10:01:43 AM
+// Search in Rotated Sorted Array
+class Solution {
+    public int search(int[] nums, int target) {
+        int n = nums.length;
+        int low = 0;
+        int high = n - 1;
+        while(low < high) {
+            int mid = (low + high) / 2;
+            if(nums[mid] > nums[n - 1]) 
+            low = mid + 1;
+            else 
+            high = mid;
+        }
+        int rot = low;
+        low = 0 ; high = n - 1;
+        while(low <= high) {
+            int mid = (low + high) / 2;
+            int real = (mid + rot) % n;
+            if(nums[real] == target)
+            return real;
+            if(nums[real] < target)
+            low = mid + 1;
+            else 
+            high = mid - 1;
+        }
+        return -1;
+    }
+}
