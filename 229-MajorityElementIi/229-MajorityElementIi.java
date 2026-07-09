59432c6da6dfffb75d83eaa0105124a1aa490b08
RAGA SANDHIYA R <ragasandhiya05@gmail.com>
2026-07-09 09:58:22 +0530

    Create 229-MajorityElementIi

---
+++ 229-MajorityElementIi/229-MajorityElementIi.java
@@ -0,0 +1,29 @@
+// Last updated: 7/9/2026, 9:58:21 AM
+// Majority Element II
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+        List<Integer> result = new ArrayList<>();
+        if(nums == null || nums.length == 0) {
+            return result;
+        }
+        Arrays.sort(nums);
+        int n = nums.length;
+        int count = 1;
+        for(int i = 1 ; i < n ; i++) {
+            if(nums[i] == nums[i - 1]) {
+                count++;
+            }
+            else {
+                if(count > n / 3) {
+                    result.add(nums[i - 1]);
+                }
+                count = 1;
+            }
+        }
+        if(count > n / 3) {
+            result.add(nums[n - 1]);
+        }
+        return result;
+    }
+}
+        
