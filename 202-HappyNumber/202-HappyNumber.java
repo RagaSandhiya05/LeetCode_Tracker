671f0cb43fe623b8a72e0005a912703bed1d83ed
RAGA SANDHIYA R <ragasandhiya05@gmail.com>
2026-07-09 09:58:40 +0530

    Create 202-HappyNumber

---
+++ 202-HappyNumber/202-HappyNumber.java
@@ -0,0 +1,20 @@
+// Last updated: 7/9/2026, 9:58:38 AM
+// Happy Number
+class Solution {
+    public boolean isHappy(int n) {
+        if(n == 1 || n == 7)
+        return true;
+        else if(n < 10)
+        return false;
+        else {
+            int sum = 0;
+            while(n > 0) {
+                int temp = n % 10;
+                sum = sum + temp * temp;
+                n = n / 10;
+            }
+            return isHappy(sum);
+        }
+    }
+}
+           
