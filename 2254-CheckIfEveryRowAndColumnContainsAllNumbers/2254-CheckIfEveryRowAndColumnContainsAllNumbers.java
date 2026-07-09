d76c0a945b2031dcf2695a97789908a435e51e56
RAGA SANDHIYA R <ragasandhiya05@gmail.com>
2026-07-09 09:51:52 +0530

    Create 2254-CheckIfEveryRowAndColumnContainsAllNumbers

---
+++ 2254-CheckIfEveryRowAndColumnContainsAllNumbers/2254-CheckIfEveryRowAndColumnContainsAllNumbers.java
@@ -0,0 +1,17 @@
+// Last updated: 7/9/2026, 9:51:51 AM
+class Solution {
+    public boolean checkValid(int[][] matrix) {
+        int len = matrix.length;
+        for(int i = 0 ; i < len ; i++) {
+            HashSet<Integer> Row = new HashSet<>();
+            HashSet<Integer> Column = new HashSet<>();
+            for(int j = 0 ; j < len ; j++) {
+                Row.add(matrix[i][j]);
+                Column.add(matrix[j][i]);
+            }
+            if(Row.size() != len || Column.size() != len)
+                return false;
+        }
+            return true;
+    }
+}
