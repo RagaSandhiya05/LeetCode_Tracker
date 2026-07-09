98ad8a294b4eff3fdaad131e85989f5acea67807
RAGA SANDHIYA R <ragasandhiya05@gmail.com>
2026-07-09 09:59:08 +0530

    Create 151-ReverseWordsInAString

---
+++ 151-ReverseWordsInAString/151-ReverseWordsInAString.java
@@ -0,0 +1,26 @@
+// Last updated: 7/9/2026, 9:59:07 AM
+// Reverse Words in a String
+class Solution {
+    public String reverseWords(String s) {
+        s = s.trim();
+        StringBuilder result = new StringBuilder();
+        int i = s.length() - 1;
+        int j = s.length() - 1;
+        while(i >= 0) {
+            if(s.charAt(i) == ' ') {
+                i--;
+                continue;
+            }
+            j = i;
+            while(i >= 0 && s.charAt(i) != ' ') {
+                i--;
+            }
+            if(result.length() > 0) {
+                result.append(" ");
+            }
+            result.append(s.substring(i + 1 , j + 1));
+        }
+        return result.toString();
+    }
+}
+           
