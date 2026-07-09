4063a75997861f34a537bf2a84820e2c0174dd99
RAGA SANDHIYA R <ragasandhiya05@gmail.com>
2026-07-09 10:02:16 +0530

    Create 9-PalindromeNumber

---
+++ 9-PalindromeNumber/9-PalindromeNumber.java
@@ -0,0 +1,18 @@
+// Last updated: 7/9/2026, 10:02:15 AM
+// Palindrome Number
+class Solution {
+    public boolean isPalindrome(int x) {
+        int original = x;
+        int rev = 0;
+        while(x > 0) {
+            int digit = x % 10;
+            rev = rev * 10 + digit;
+            x = x / 10;
+        }
+        if(original == rev)
+        return true;
+        else
+        return false;
+    }
+}
+   
