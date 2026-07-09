9a73a93bfa7a92a65b7d3743c823d771c91bc4ba
RAGA SANDHIYA R <ragasandhiya05@gmail.com>
2026-07-09 09:53:06 +0530

    Create 1933-NumberOfDifferentIntegersInAString

---
+++ 1933-NumberOfDifferentIntegersInAString/1933-NumberOfDifferentIntegersInAString.java
@@ -0,0 +1,25 @@
+// Last updated: 7/9/2026, 9:53:04 AM
+// Number of Different Integers in a String
+class Solution {
+    public int numDifferentIntegers(String word) {
+        Set<String> set = new HashSet<>();
+        int start = 0;
+        int length = word.length();
+        while(start < length) {
+            if(!Character.isDigit(word.charAt(start))) {
+                start++;
+                continue;
+            }
+            int end = start;
+            while(end < length && Character.isDigit(word.charAt(end))) {
+                end++;
+            }
+            while(start < end && word.charAt(start) == '0') {
+                start++;
+            }
+            set.add(word.substring(start , end));
+            start = end;
+            }
+            return set.size();
+    }
+}
