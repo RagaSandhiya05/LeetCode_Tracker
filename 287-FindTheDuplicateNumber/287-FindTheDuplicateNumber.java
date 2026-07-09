75b0adbd752da8a8226399652109ffa90ef7d195
RAGA SANDHIYA R <ragasandhiya05@gmail.com>
2026-07-09 09:58:08 +0530

    Create 287-FindTheDuplicateNumber

---
+++ 287-FindTheDuplicateNumber/287-FindTheDuplicateNumber.java
@@ -0,0 +1,17 @@
+// Last updated: 7/9/2026, 9:58:07 AM
+// Find the Duplicate Number
+class Solution {
+    public int findDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for(int num : nums) {
+            if(!set.contains(num)) {
+                set.add(num);
+            }
+            else {
+                return num;
+            }
+        }
+        return -1;
+    }
+}
+           
