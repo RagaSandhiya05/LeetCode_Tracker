a3df8e58ec9bb9cd066aae00e94fd160c89d2607
RAGA SANDHIYA R <ragasandhiya05@gmail.com>
2026-07-09 09:57:33 +0530

    Create 395-LongestSubstringWithAtLeastKRepeatingCharacters

---
+++ 395-LongestSubstringWithAtLeastKRepeatingCharacters/395-LongestSubstringWithAtLeastKRepeatingCharacters.java
@@ -0,0 +1,20 @@
+// Last updated: 7/9/2026, 9:57:31 AM
+// Longest Substring with At Least K Repeating Characters
+class Solution {
+    public int longestSubstring(String s, int k) {
+        int n = s.length();
+        int freq[] = new int[26];
+        for(char ch : s.toCharArray()) {
+            freq[ch - 'a']++;
+        }
+        for(int i = 0 ; i < n ; i++) {
+            if(freq[s.charAt(i) - 'a'] < k) {
+                int left = longestSubstring(s.substring(0 , i) , k);
+                int right = longestSubstring(s.substring(i + 1) , k);
+                return Math.max(left , right);
+            }
+        }
+        return n;
+    }
+}
+          
