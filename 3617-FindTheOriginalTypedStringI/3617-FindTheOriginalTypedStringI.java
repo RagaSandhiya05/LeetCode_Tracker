92833d96af598a0b3a447ad1f9418c7d717a0758
RAGA SANDHIYA R <ragasandhiya05@gmail.com>
2026-07-09 09:48:25 +0530

    Create 3617-FindTheOriginalTypedStringI

---
+++ 3617-FindTheOriginalTypedStringI/3617-FindTheOriginalTypedStringI.java
@@ -0,0 +1,20 @@
+// Last updated: 7/9/2026, 9:48:23 AM
+class Solution {
+    public int possibleStringCount(String word) {
+        int ans = 1;
+        int count = 1;
+        char last = word.charAt(0);
+        for(int i = 1 ; i < word.length() ; i++) {
+            if(word.charAt(i) != last) {
+                ans += count - 1;
+                count = 1;
+                last = word.charAt(i);
+            } else {
+                count++;
+            }
+            }
+            ans += count - 1;
+            return ans;
+    }
+}
+    
