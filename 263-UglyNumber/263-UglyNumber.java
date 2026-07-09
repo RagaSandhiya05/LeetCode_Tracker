ecf93b6f961ad58b852b17d02636daecf6e868bd
RAGA SANDHIYA R <ragasandhiya05@gmail.com>
2026-07-09 09:58:13 +0530

    Create 263-UglyNumber

---
+++ 263-UglyNumber/263-UglyNumber.java
@@ -0,0 +1,20 @@
+// Last updated: 7/9/2026, 9:58:12 AM
+// Ugly Number
+class Solution {
+    public boolean isUgly(int n) {
+        if(n <= 0)
+        return false;
+        while(n != 1) {
+            if(n % 2 == 0)
+            n = n / 2;
+            else if(n % 3 == 0)
+            n = n / 3;
+            else if(n % 5 == 0)
+            n = n / 5;
+            else
+            return false;
+        }
+        return true;
+    }
+}
+      
