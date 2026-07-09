b212ef46a5cb2bd97d0351f79110e38c582f7dc4
RAGA SANDHIYA R <ragasandhiya05@gmail.com>
2026-07-09 09:49:38 +0530

    Create 3338-CountSubmatricesWithTopLeftElementAndSumLessThanK

---
+++ 3338-CountSubmatricesWithTopLeftElementAndSumLessThanK/3338-CountSubmatricesWithTopLeftElementAndSumLessThanK.java
@@ -0,0 +1,26 @@
+// Last updated: 7/9/2026, 9:49:37 AM
+// Count Submatrices with Top-Left Element and Sum Less Than k
+class Solution {
+    public int countSubmatrices(int[][] grid, int k) {
+        int m = grid.length , n = grid[0].length;
+        int count = 0;
+        for(int i = 0 ; i < m ; i++) {
+            for(int j = 0 ; j < n ; j++) {
+                if(i > 0) {
+                    grid[i][j] = grid[i][j] + grid[i - 1][j];
+                }
+                if(j > 0) {
+                    grid[i][j] = grid[i][j] + grid[i][j - 1];
+                }
+                if(i > 0 && j > 0) {
+                    grid[i][j] = grid[i][j] - grid[i - 1][j - 1];
+                }
+                if(grid[i][j] <= k) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
+               
