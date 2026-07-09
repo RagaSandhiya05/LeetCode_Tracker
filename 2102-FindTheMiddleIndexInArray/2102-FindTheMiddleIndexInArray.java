ff95a5a8a33c7f54e0add9ab7a8c4ec17f9e9fa0
RAGA SANDHIYA R <ragasandhiya05@gmail.com>
2026-07-09 09:52:27 +0530

    Create 2102-FindTheMiddleIndexInArray

---
+++ 2102-FindTheMiddleIndexInArray/2102-FindTheMiddleIndexInArray.java
@@ -0,0 +1,20 @@
+// Last updated: 7/9/2026, 9:52:26 AM
+// Find the Middle Index in Array
+class Solution {
+    public int findMiddleIndex(int[] nums) {
+        int n = nums.length;
+        int sum = 0;
+        for(int i = 0 ; i < n ; i++) {
+            sum = sum + nums[i];
+        }
+        int left = 0;
+        for(int i = 0 ; i < n ; i++) {
+            sum = sum - nums[i];
+            if(left == sum) {
+                return i;
+            }
+            left = left + nums[i];
+        }
+        return -1;
+    }
+}
