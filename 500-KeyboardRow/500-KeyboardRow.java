1213f633334f9de20e97772757148001d86a1d3a
RAGA SANDHIYA R <ragasandhiya05@gmail.com>
2026-07-09 09:56:47 +0530

    Create 500-KeyboardRow

---
+++ 500-KeyboardRow/500-KeyboardRow.java
@@ -0,0 +1,23 @@
+// Last updated: 7/9/2026, 9:56:46 AM
+// Keyboard Row
+class Solution {
+    public String[] findWords(String[] words) {
+        ArrayList<String> ans = new ArrayList<>();
+        String first = "qwertyuiop";
+        String second = "asdfghjkl";
+        String third = "zxcvbnm";
+        for(String i : words) {
+        if(isinrow(i , first) || isinrow(i , second) || isinrow(i , third)) 
+            ans.add(i);
+        }
+        return ans.toArray(new String[0]);
+    }
+    private boolean isinrow(String s , String row) {
+        for(char c : s.toCharArray()) {
+            if(row.indexOf(Character.toLowerCase(c)) == -1) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
