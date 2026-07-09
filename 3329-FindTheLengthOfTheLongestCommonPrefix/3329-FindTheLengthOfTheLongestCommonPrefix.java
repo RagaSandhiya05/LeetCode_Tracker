2460a1c21db5e50e6018c621b14c9f9ff70867ab
RAGA SANDHIYA R <ragasandhiya05@gmail.com>
2026-07-09 09:49:51 +0530

    Create 3329-FindTheLengthOfTheLongestCommonPrefix

---
+++ 3329-FindTheLengthOfTheLongestCommonPrefix/3329-FindTheLengthOfTheLongestCommonPrefix.java
@@ -0,0 +1,27 @@
+// Last updated: 7/9/2026, 9:49:50 AM
+// Find the Length of the Longest Common Prefix
+class Solution {
+    public int longestCommonPrefix(int[] arr1, int[] arr2) {
+        Set<Integer> prefix = new HashSet<>();
+        for(int a : arr1) {
+            while(a > 0) {
+                if(prefix.contains(a))
+                break;
+                prefix.add(a);
+                a = a / 10;
+            }
+        }
+        int res = 0;
+        for(int b : arr2) {
+            while(b > res) {
+                if(prefix.contains(b)) {
+                    res = b;
+                    break;
+                }
+                b = b / 10;
+            }
+        }
+        return res == 0 ? 0 : String.valueOf(res).length();
+    }
+}
+              
