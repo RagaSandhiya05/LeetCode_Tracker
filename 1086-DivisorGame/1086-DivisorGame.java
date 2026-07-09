d72b93d33b7f19777e888848f31b09180567b4dd
RAGA SANDHIYA R <ragasandhiya05@gmail.com>
2026-07-09 09:55:18 +0530

    Create 1086-DivisorGame

---
+++ 1086-DivisorGame/1086-DivisorGame.java
@@ -0,0 +1,11 @@
+// Last updated: 7/9/2026, 9:55:16 AM
+class Solution {
+    public boolean divisorGame(int n) {
+        if(n % 2 == 0){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
