c270c9c823beade700f4627328458087b21ed4f1
RAGA SANDHIYA R <ragasandhiya05@gmail.com>
2026-07-09 09:56:05 +0530

    Create 782-JewelsAndStones

---
+++ 782-JewelsAndStones/782-JewelsAndStones.java
@@ -0,0 +1,15 @@
+// Last updated: 7/9/2026, 9:56:04 AM
+// Jewels and Stones
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        int count = 0;
+        for(char j : jewels.toCharArray()) {
+            for(char s : stones.toCharArray()) {
+                if(j == s) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
