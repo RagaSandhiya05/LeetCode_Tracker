7c82ff8325d58220cd1d309b8b86b612d6ee7798
RAGA SANDHIYA R <ragasandhiya05@gmail.com>
2026-07-09 09:57:23 +0530

    Create 412-FizzBuzz

---
+++ 412-FizzBuzz/412-FizzBuzz.java
@@ -0,0 +1,23 @@
+// Last updated: 7/9/2026, 9:57:22 AM
+// Fizz Buzz
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        List<String> ans = new ArrayList<>();
+        for(int i = 1 ; i <= n ; i++) {
+            if(i % 3 == 0 && i % 5 == 0) {
+                ans.add("FizzBuzz");
+            }
+            else if(i % 3 == 0) {
+                ans.add("Fizz");
+            }
+            else if(i % 5 == 0) {
+                ans.add("Buzz");
+            }
+            else {
+                ans.add(Integer.toString(i));
+            }
+        }
+        return ans;
+    }
+}
+           
