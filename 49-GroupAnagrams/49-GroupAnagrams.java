4540d81d2a5fc2c7905a60051c9bceb751f1ea4b
RAGA SANDHIYA R <ragasandhiya05@gmail.com>
2026-07-09 10:01:32 +0530

    Create 49-GroupAnagrams

---
+++ 49-GroupAnagrams/49-GroupAnagrams.java
@@ -0,0 +1,18 @@
+// Last updated: 7/9/2026, 10:01:31 AM
+// Group Anagrams
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String , List<String>> map = new HashMap<>();
+        for(String word : strs) {
+            char[] chars = word.toCharArray();
+            Arrays.sort(chars);
+            String sortedWord = new String(chars);
+            if(!map.containsKey(sortedWord)) {
+                map.put(sortedWord , new ArrayList<>());
+            }
+            map.get(sortedWord).add(word);
+        }
+        return new ArrayList<>(map.values());
+    }
+}
+          
