cbed3f4de372868017470aec0b2e8ca8fd53c1a8
RAGA SANDHIYA R <ragasandhiya05@gmail.com>
2026-07-09 10:02:15 +0530

    Create 10-RegularExpressionMatching

---
+++ 10-RegularExpressionMatching/10-RegularExpressionMatching.java
@@ -0,0 +1,17 @@
+// Last updated: 7/9/2026, 10:02:14 AM
+class Solution {
+    public boolean isMatch(String text, String pattern) {
+        if (pattern.isEmpty()) return text.isEmpty();
+        boolean first_match = (!text.isEmpty() && (pattern.charAt(0) == text.charAt(0) || pattern.charAt(0) == '.'));
+        if(pattern.length() >= 2 && pattern.charAt(1) == '*') {
+            return (
+                isMatch(text , pattern.substring(2)) || 
+                (first_match && isMatch(text.substring(1) , pattern))
+                );
+            } else {
+                return (
+                    first_match && isMatch(text.substring(1) , pattern.substring(1))
+                );
+            }
+        }
+    }
