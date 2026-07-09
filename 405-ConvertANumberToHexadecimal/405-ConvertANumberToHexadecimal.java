097d5d87b0121fd8148c3a97e09bc050b1b9557a
RAGA SANDHIYA R <ragasandhiya05@gmail.com>
2026-07-09 09:57:28 +0530

    Create 405-ConvertANumberToHexadecimal

---
+++ 405-ConvertANumberToHexadecimal/405-ConvertANumberToHexadecimal.java
@@ -0,0 +1,7 @@
+// Last updated: 7/9/2026, 9:57:26 AM
+class Solution {
+    public String toHex(int num) {
+        String hexString = Integer.toHexString(num);
+        return hexString;
+    }
+}
