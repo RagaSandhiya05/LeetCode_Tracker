f694a91b968b4df39934855a174e63aa5949f51c
RAGA SANDHIYA R <ragasandhiya05@gmail.com>
2026-07-09 10:01:55 +0530

    Create 24-SwapNodesInPairs

---
+++ 24-SwapNodesInPairs/24-SwapNodesInPairs.java
@@ -0,0 +1,12 @@
+// Last updated: 7/9/2026, 10:01:53 AM
+
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        if(head == null || head.next == null) return head;
+        ListNode temp = head.next;
+        head.next = swapPairs(temp.next);
+        temp.next = head;
+        return temp;
+    }
+}
+        
