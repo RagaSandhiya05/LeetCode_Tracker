2c7105e84c165e3d018dc4e08a802a95c4bd9f4d
RAGA SANDHIYA R <ragasandhiya05@gmail.com>
2026-07-09 09:55:23 +0530

    Create 1054-ComplementOfBase10Integer

---
+++ 1054-ComplementOfBase10Integer/1054-ComplementOfBase10Integer.java
@@ -0,0 +1,20 @@
+// Last updated: 7/9/2026, 9:55:22 AM
+// Complement of Base 10 Integer
+class Solution {
+    public int bitwiseComplement(int n) {
+        if(n == 0) 
+            return 1;
+            int ans = 0;
+            int i = 0;
+            while(n > 0) {
+                int bit = n % 2;
+                if(bit == 0) {
+                    ans += Math.pow(2 , i);
+                }
+                i++;
+                n = n / 2;
+            }
+            return ans;
+    }
+}
+               
