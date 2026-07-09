11afa4d4b75e832fd801ef479bd8973c3adae765
RAGA SANDHIYA R <ragasandhiya05@gmail.com>
2026-07-09 09:52:54 +0530

    Create 1984-MaximumDistanceBetweenAPairOfValues

---
+++ 1984-MaximumDistanceBetweenAPairOfValues/1984-MaximumDistanceBetweenAPairOfValues.java
@@ -0,0 +1,11 @@
+// Last updated: 7/9/2026, 9:52:53 AM
+// Maximum Distance Between a Pair of Values
+class Solution {
+    public int maxDistance(int[] nums1, int[] nums2) {
+        int i , j;
+        for(i = 0 , j = 0 ; i < nums1.length && j < nums2.length ; j++)
+        if(nums1[i] > nums2[j])
+        i++;
+        return Math.max(0 , j - i - 1);
+    }
+}
