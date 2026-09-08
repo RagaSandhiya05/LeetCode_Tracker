730cd9ce3d7a8f1a2a6206e2684f193955ed6e7f
RAGA SANDHIYA R <ragasandhiya05@gmail.com>
2026-09-08 08:51:45 +0530

    Create 3870-CountCommasinRange

---
+++ 3870-CountCommasinRange/version/java/3870-CountCommasinRange_20260908_085141.java
@@ -0,0 +1,13 @@
+// Last updated: 9/8/2026, 8:51:41 AM
+1// Count Commas in Range
+2class Solution {
+3    public int countCommas(int n) {
+4        int count = 0;
+5        for(int i = 0 ; i <= n ; i++) {
+6            if(i >= 1000) {
+7                count++;
+8            }
+9        }
+10        return count;
+11    }
+12}
